e89592aa5bed9282358810cd92979157ef204c55
Max Paulo <maxpauloteixeira@outlook.com>
2026-04-01 00:21:59 -0300

    create Menu class

--- src/br/com/cinema/Main.java
+++ src/br/com/cinema/Main.java
@@ -1,8 +1,12 @@
 package br.com.cinema;
 
+import br.com.cinema.ui.Menu;
+
 public class Main {
 
     void main(){
-        
+        Menu menu = new Menu();
+
+        menu.iniciaMenu();
     }
 }
---
+++ src/br/com/cinema/ui/Menu.java
@@ -0,0 +1,9 @@
+package br.com.cinema.ui;
+
+public class Menu {
+
+    public void iniciaMenu() {
+        System.out.println("Bem vindo ao cinema-admin!");
+    }
+
+}
